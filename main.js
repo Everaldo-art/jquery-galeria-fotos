0a85fa2b91fe85f258bbfc2c1171c929c040eb14
Everaldo J Alves <Everaldo j Alves>
2024-05-29 14:57:42 -0300

    adicionei index.html, main.css,main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+$('header button').click(function(){
+
+alert("Expandir formulario")
+})
+
+$('form').on ('submit', function(e) {
+    console.log("submit");
+    e.preventDefaut();
+})
+})
+
