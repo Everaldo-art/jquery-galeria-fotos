e57469a3bf42f1ad407a9ea7f09dffd9076b2f70
Everaldo J Alves <Everaldo j Alves>
2024-06-27 12:11:36 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-$(document).ready(function(){
-    
-$('header button').click(function(){
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    });
 
-alert("Expandir formulario")
-})
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    });
 
-$('form').on ('submit', function(e) {
-    console.log("submit");
-    e.preventDefaut();
-})
-})
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none"></li>');
 
+        $('<img />', {
+            src: enderecoDaNovaImagem
+        }).appendTo(novoItem);
+
+        $('<div class="overlay-imagem-link"></div>')
+            .html(`
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            `)
+            .appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+
+        $('#endereco-imagem-nova').val('');
+    });
+});
